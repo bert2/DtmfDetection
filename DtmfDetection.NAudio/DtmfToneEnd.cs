33b26b181f6d11f3cf5eb66b0897e7f3cc386394
Robert Hofmann <roberthofmann2@outlook.de>
2016-10-12 21:05:17 +0200

    Make DtmfToneStart/End equatable.

--- DtmfDetection.NAudio/DtmfToneEnd.cs
+++ DtmfDetection.NAudio/DtmfToneEnd.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public class DtmfToneEnd
+    public class DtmfToneEnd : IEquatable<DtmfToneEnd>
     {
         public DtmfToneEnd(DtmfTone dtmfTone, int channel, TimeSpan duration)
         {
@@ -13,8 +13,30 @@
 
         public DtmfTone DtmfTone { get; }
 
-        public int Channel { get; private set; }
+        public int Channel { get; }
 
         public TimeSpan Duration { get; }
+
+        #region Equality implementations
+
+        public override bool Equals(object obj) => !ReferenceEquals(obj, null) && Equals(obj as DtmfToneEnd);
+
+        public bool Equals(DtmfToneEnd other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return DtmfTone == other.DtmfTone
+                && Channel == other.Channel
+                && Duration == other.Duration;
+        }
+
+        public override int GetHashCode() => new { DtmfTone, Channel, Duration }.GetHashCode();
+
+        public static bool operator ==(DtmfToneEnd a, DtmfToneEnd b) => ReferenceEquals(a, null) ? ReferenceEquals(b, null) : a.Equals(b);
+
+        public static bool operator !=(DtmfToneEnd a, DtmfToneEnd b) => !(a == b);
+
+        #endregion
     }
 }
--- DtmfDetection.NAudio/DtmfToneStart.cs
+++ DtmfDetection.NAudio/DtmfToneStart.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public class DtmfToneStart
+    public class DtmfToneStart : IEquatable<DtmfToneStart>
     {
         public DtmfToneStart(DtmfTone dtmfTone, int channel, DateTime position)
         {
@@ -16,5 +16,27 @@
         public int Channel { get; }
 
         public DateTime Position { get; }
+
+        #region Equality implementations
+
+        public override bool Equals(object obj) => !ReferenceEquals(obj, null) && Equals(obj as DtmfToneStart);
+
+        public bool Equals(DtmfToneStart other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return DtmfTone == other.DtmfTone
+                && Channel == other.Channel
+                && Position == other.Position;
+        }
+
+        public override int GetHashCode() => new { DtmfTone, Channel, Position }.GetHashCode();
+
+        public static bool operator ==(DtmfToneStart a, DtmfToneStart b) => ReferenceEquals(a, null) ? ReferenceEquals(b, null) : a.Equals(b);
+
+        public static bool operator !=(DtmfToneStart a, DtmfToneStart b) => !(a == b);
+
+        #endregion
     }
 }
