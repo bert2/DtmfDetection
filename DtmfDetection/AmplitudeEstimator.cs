070e7d4510ea511d31f7b17164a5a64195e57e25
bert2 <shuairan@gmail.com>
2018-01-13 13:26:14 +0100

    Get rid of unnecessary Lazy wrapper.

--- DtmfDetection/AmplitudeEstimator.cs
+++ DtmfDetection/AmplitudeEstimator.cs
@@ -10,16 +10,13 @@
 
         private double s2;
 
-        private Lazy<double> amplitude;
-
         public AmplitudeEstimator(double targetFrequency, double sampleRate, int numberOfSamples)
         {
-            Reset();
             var k = Math.Round(targetFrequency / sampleRate * numberOfSamples);
             c = 2.0 * Math.Cos(2.0 * Math.PI * k / numberOfSamples);
         }
 
-        public double AmplitudeSquared => amplitude.Value;
+        public double AmplitudeSquared => s1*s1 + s2*s2 - s1*s2*c;
 
         public void Add(float sample)
         {
@@ -31,7 +28,6 @@
         public void Reset()
         {
             s1 = s2 = .0;
-            amplitude = new Lazy<double>(() => s1*s1 + s2*s2 - s1*s2*c);
         }
     }
 }
