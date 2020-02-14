4f836c1b76566ec842478cd9575971b2035bccbf
bert2 <shuairan@gmail.com>
2020-02-14 16:00:28 +0100

    let clients feed `Config` if needed

--- src/DtmfDetection.NAudio/WaveStreamExtensions.cs
+++ src/DtmfDetection.NAudio/WaveStreamExtensions.cs
@@ -3,11 +3,18 @@
     using global::NAudio.Wave;
 
     public static class WaveStreamExtensions {
-        public static List<DtmfChange> DtmfChanges(this WaveStream waveStream, bool forceMono = true) {
-            var a = Analyzer.Create(new AudioFile(waveStream, Config.Default.SampleRate, forceMono), Config.Default);
-            var ds = new List<DtmfChange>();
-            while (a.MoreSamplesAvailable) ds.AddRange(a.AnalyzeNextBlock());
-            return ds;
+        public static List<DtmfChange> DtmfChanges(this WaveStream waveStream, bool forceMono = true, in Config? config = null) {
+            var cfg = config ?? Config.Default;
+            var analyzer = Analyzer.Create(
+                new AudioFile(waveStream, cfg.SampleRate, forceMono),
+                cfg);
+
+            var dtmfs = new List<DtmfChange>();
+
+            while (analyzer.MoreSamplesAvailable)
+                dtmfs.AddRange(analyzer.AnalyzeNextBlock());
+
+            return dtmfs;
         }
     }
 }
